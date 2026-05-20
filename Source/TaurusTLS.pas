b04b4382c1bbbdc250578e686050f34b98a81a33
Alexander Tregubov (aka (@)AT) <tregubovav.dev@gmail.com>
2026-05-19 17:29:52 -0700

    Fix for masking SIGPIPE system exception in Linux
    The SIGPIPE system exception crashes the application.
    This may happen when the socket tries to read or write data when the
    peer restets connection.
    In TaurusTLS case this happens when the client resets the TCP connection
    right after the handshake.
    Unfortunately Indy does not mask the SIGPIPE exception for Linux yet.
    The fix adds SIGPIPE signal masking per worker thread explicitly in
    Connect and Accept methods.
    
    P.S.
    We will infrom Indy developers about the issue and will propose fix
    directly to Indy library.

--- Source/TaurusTLS.pas
+++ Source/TaurusTLS.pas
@@ -235,6 +235,9 @@ uses
   Windows,
   {$ENDIF}
 {$ENDIF}
+{$IF Defined(Linux) or Defined(Android)}
+  Posix.Signal,
+{$IFEND}
   Classes,
   IdCTypes,
   IdGlobal,
@@ -1285,6 +1288,9 @@ type
     fSSLContext: TTaurusTLSContext;
     fHostName: String;
     fVerifyHostname: Boolean;
+{$IF Defined(Linux) or Defined(Android)}
+     FSigSet: sigset_t;
+{$IFEND}
     function GetSSLProtocolVersion: TTaurusTLSSSLVersion;
     function GetSSLProtocolVersionStr: string;
     function GetPeerCert: TTaurusTLSX509;
@@ -1292,6 +1298,9 @@ type
     function GetCipher: TTaurusTLSCipher;
     function GetVerifyHostname: Boolean;
     procedure SetVerifyHostName(const Value: Boolean);
+{$IF Defined(Linux) or Defined(Android)}
+    procedure DisableSigPipe;
+{$IFEND}
   public
     /// <summary>
     /// Creates a new instance of TTaurusTLSSocket.
@@ -4900,6 +4909,9 @@ var
 begin
   Assert(fSSL = nil);
   Assert(fSSLContext <> nil);
+{$IF Defined(Linux) or Defined(Android)}
+  DisableSigPipe;
+{$IFEND}
   fSSL := SSL_new(fSSLContext.Context);
   if fSSL = nil then
   begin
@@ -4969,6 +4981,9 @@ var
 begin
   Assert(fSSL = nil);
   Assert(fSSLContext <> nil);
+{$IF Defined(Linux) or Defined(Android)}
+  DisableSigPipe;
+{$IFEND}
   if Supports(FParent, ITaurusTLSCallbackHelper, IInterface(LHelper)) then
   begin
     LParentIO := LHelper.GetIOHandlerSelf;
@@ -5226,6 +5241,15 @@ procedure TTaurusTLSSocket.SetVerifyHostName(const Value: Boolean);
 begin
   fVerifyHostname := Value;
 end;
+
+{$IF Defined(Linux) or Defined(Android)}
+procedure TTaurusTLSSocket.DisableSigPipe;
+begin
+  sigemptyset(FSigSet);
+  sigaddset(FSigSet, SIGPIPE);
+  pthread_sigmask(SIG_BLOCK, @FSigSet, nil);
+end;
+{$IFEND}
 
 function TTaurusTLSSocket.GetSSLProtocolVersion: TTaurusTLSSSLVersion;
 begin
